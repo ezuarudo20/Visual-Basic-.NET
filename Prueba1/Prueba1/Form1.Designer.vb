1dbdf7e53b848db56b4d4ca494d81d79c50d95ec
ezuarudo20 <ezuarudo@gmail.com>
2015-08-29 14:51:55 -0500

    Manejo de excepción en Visual Basic .Net
    Leer e imprimir un numero entero que si al poner una letra me salga un
    mensaje de error y la descripción del mismo.
    
    Nombre del proyecto: Prueba1

---
+++ Prueba1/Prueba1/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtbox1 = New System.Windows.Forms.TextBox()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(126, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Mostrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtbox1
+        '
+        Me.txtbox1.Location = New System.Drawing.Point(126, 35)
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(91, 20)
+        Me.txtbox1.TabIndex = 1
+        Me.txtbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(126, 13)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(91, 13)
+        Me.lbl1.TabIndex = 2
+        Me.lbl1.Text = "Introducir Numero"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 139)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Leer Numero Entero"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtbox1 As System.Windows.Forms.TextBox
+    Friend WithEvents lbl1 As System.Windows.Forms.Label
+
+End Class
---
+++ Prueba1/Prueba1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+       
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1 As Integer
+        Try
+            num1 = txtbox1.Text
+            MsgBox(num1, MsgBoxStyle.Information, "Lectura de Numero entero"
+                                             )
+        Catch ex As Exception
+            MsgBox("NO ES UN NUMERO ENTERO", MsgBoxStyle.Critical, "Alerta")
+            MsgBox(ex.ToString)
+        End Try
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
