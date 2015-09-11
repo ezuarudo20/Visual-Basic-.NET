17ef121212bcbea23596d09e9902dbfb8450e319
ezuarudo20 <ezuarudo@gmail.com>
2015-09-11 15:14:28 -0500

    Factorial de un numero en visual basic
    factorial de un numero impreso en un msgbox.information

---
+++ WindowsApplication3/WindowsApplication3/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Integer
+        a = Val(TextBox1.Text)
+        b = 1
+        For c = 1 To a
+            b = b * c
+
+
+
+        Next
+        MsgBox(b)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
